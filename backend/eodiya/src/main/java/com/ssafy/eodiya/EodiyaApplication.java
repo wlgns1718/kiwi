cdf42f4dcb25f07572df873373b3dce6115a4069
신지훈 <wlgns1718@naver.com>
2023-05-14 16:14:19 +0900

    init : backend project

---
+++ backend/eodiya/src/main/java/com/ssafy/eodiya/EodiyaApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.eodiya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EodiyaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EodiyaApplication.class, args);
+	}
+
+}
