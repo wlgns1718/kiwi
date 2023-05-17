4474d3acfa4e2f6056ae2ad4421c48e5bf2f5d26
squareyun <squareyun@gmail.com>
2023-05-17 20:35:23 +0900

    feat: (back) Add board get list controller

--- backend/eodiya/src/main/java/com/ssafy/kiwi/board/controller/BoardController.java
+++ backend/eodiya/src/main/java/com/ssafy/kiwi/board/controller/BoardController.java
@@ -4,14 +4,20 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ssafy.kiwi.board.model.BoardDto;
 import com.ssafy.kiwi.board.service.BoardService;
 import com.ssafy.kiwi.tour.controller.TourController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @Controller
 @RequestMapping("/board")
@@ -26,11 +32,16 @@ public class BoardController {
 		this.boardService = boardService;
 	}
 	
-	@GetMapping("/list")
-	public String list(Model model) throws Exception {
-		logger.debug("board list call parameter");
-//		List<BoardDto> list = boardService.getList();
-//		model.addAttribute("articles", list);
-		return "board/list";
+	@GetMapping
+	public ResponseEntity<List<BoardDto>> listArticle() throws Exception {
+		logger.info("listArticle - 호출");
+		return new ResponseEntity<List<BoardDto>>(boardService.getList(), HttpStatus.OK);
 	}
+	
+//	@GetMapping("/{articleno}")
+//	public ResponseEntity<BoardDto> getArticle(@PathVariable("articleno") @ApiParam(value = "얻어올 글의 글번호.", required = true) int articleno) throws Exception {
+//		logger.info("getArticle - 호출 : " + articleno);
+//		boardService.updateHit(articleno);
+//		return new ResponseEntity<BoardDto>(boardService.getArticle(articleno), HttpStatus.OK);
+//	}
 }
--- backend/eodiya/src/main/java/com/ssafy/kiwi/board/model/BoardDto.java
+++ backend/eodiya/src/main/java/com/ssafy/kiwi/board/model/BoardDto.java
@@ -12,49 +12,61 @@ public class BoardDto {
 	private int hit;
 	private int scope;
 	private int userno;
+
 	public int getBoardno() {
 		return boardno;
 	}
+
 	public void setBoardno(int boardno) {
 		this.boardno = boardno;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public String getCreatedate() {
 		return createdate;
 	}
+
 	public void setCreatedate(String createdate) {
 		this.createdate = createdate;
 	}
+
 	public int getHit() {
 		return hit;
 	}
+
 	public void setHit(int hit) {
 		this.hit = hit;
 	}
+
 	public int getScope() {
 		return scope;
 	}
+
 	public void setScope(int scope) {
 		this.scope = scope;
 	}
+
 	public int getUserno() {
 		return userno;
 	}
+
 	public void setUserno(int userno) {
 		this.userno = userno;
 	}
-
-	
 
 }
